1cd78c63c14a97142f81263e33b9150450a6d72f
Johnny <audiotale.contact@gmail.com>
2025-12-03 21:19:44 +0100

    j

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { HashRouter } from 'react-router-dom'
+import { BrowserRouter } from 'react-router-dom'
 import App from './App.jsx'
 import './styles.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <HashRouter>
+    {/* Utilisation de BrowserRouter avec le basename dynamique défini dans vite.config.js */}
+    <BrowserRouter basename={import.meta.env.BASE_URL}>
       <App />
-    </HashRouter>
+    </BrowserRouter>
   </React.StrictMode>
 )
